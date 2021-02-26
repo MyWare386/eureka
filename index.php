1cd881482d7ab5488922ca82a65cb8a5f45b629c
Martin DROULEZ <38752508+MyWare386@users.noreply.github.com>
2021-02-26 13:24:55 +0100

    All files added

---
+++ index.php
@@ -0,0 +1,287 @@
+<?php
+
+$fichier = file('francais.txt'); // Nom du fichier qui contient les citations
+
+$total = count($fichier); // Total du nombre de lignes du fichier
+
+$i = mt_rand(0, $total-1); // Nombre au hasard entre 0 et le total du nombre de lignes
+
+$wordphp = $fichier[$i];
+
+?>
+
+<!-- ----------------------------------------------------------------------------------------------------------------------------------------------------------------- -->
+
+<html lang="fr">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Eurêka</title>
+        <link rel="stylesheet" href="style.css">
+        <script src='https://kit.fontawesome.com/a076d05399.js' crossorigin='anonymous'></script>
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+        <link rel="icon" type="image/png" href="favicon.JPG" />
+        <style>
+            html, body { 
+                height: 100%; 
+            }
+
+            body {
+                overflow: auto;
+            }
+
+            .ocean { 
+                height: 5%;
+                width:100%;
+                position:fixed;
+                bottom:0;
+                left:0;
+                background: #015871;
+                z-index: -10;
+            }
+
+            .wave {
+                background: url(https://s3-us-west-2.amazonaws.com/s.cdpn.io/85486/wave.svg) repeat-x; 
+                position: absolute;
+                top: -198px;
+                width: 6400px;
+                height: 198px;
+                animation: wave 7s cubic-bezier( 0.36, 0.45, 0.63, 0.53) infinite;
+                transform: translate3d(0, 0, 0);
+                z-index: -10;
+            }
+
+            .wave:nth-of-type(2) {
+                top: -175px;
+                animation: wave 7s cubic-bezier( 0.36, 0.45, 0.63, 0.53) -.125s infinite, swell 7s ease -1.25s infinite;
+                opacity: 1;
+            }
+
+            @keyframes wave {
+                0% {
+                    margin-left: 0;
+                }
+                100% {
+                    margin-left: -1600px;
+                }
+            }
+
+            @keyframes swell {
+                0%, 100% {
+                    transform: translate3d(0,-25px,0);
+                }
+                50% {
+                    transform: translate3d(0,5px,0);
+                }
+            }
+        </style>
+    </head>
+
+    <body>
+
+        <h1 class="title"><a href="index.php"><i class='far fa-lightbulb' style='font-size:36px'></i> Eurêka</a></h1>
+
+        <br>
+        <br>
+        <br>
+
+        <acronym title="Vous avez trouvé une idée ?">
+            <h1 class="ml1">
+                <p id="word">
+                    <span class="container">
+                        <span class="text-wrapper">
+                            <span class="line line1"></span>
+                            <span class="letters">
+                                <?php echo utf8_encode($wordphp); ?>
+                            </span>
+                            <span class="line line2"></span>
+                        </span>
+                    </span>
+                </p>
+            </h1>
+        </acronym>
+
+        <button type="button" class="word" onclick="document.location.reload(false)">Un autre mot <i class='fas fa-redo' style='font-size:16px'></i></button>
+
+        <br>
+        <br>
+        <br>
+
+        <button type="button" class="eureka">Eurêka <i class='fas fa-bolt' style='font-size:16px'></i></button>
+
+        <br>
+        <br>
+        <br>
+        <br>
+
+        <div style='display:none;' id="form">
+
+            <h2 id="note">Notez votre idée <i class='far fa-edit' style='font-size:16px'></i></h2>
+            <h4 id="attention"><i class='fas fa-exclamation-triangle' style='font-size:12px;color:red'></i> Attention, si vous cliquez sur "Un autre mot", votre idée s'effacera <i class='fas fa-exclamation-triangle' style='font-size:12px;color:red'></i></h6>
+
+            <br>
+            <br>
+
+            <form action="mail.php" method="post">
+
+                <textarea class="form" name="idea" placeholder="Votre idée" cols="50" rows="15" id="tocopy"></textarea><br /><br /><br /><br />
+
+                <input type="button" value="Copier votre idée dans le presse-papier" class="js-copy" data-target="#tocopy"><br /><br /><br /><br />
+
+                <input type="submit" value="Envoyez-vous votre idée par mail" class="js-copy">
+
+            </form>
+
+            <br /><br />
+
+            <button id="slideup"><i class='fas fa-angle-double-up' style='font-size:24px'></i></button>
+
+        </div>
+
+        <footer>
+
+            <div class="separation"></div>
+
+            <br>
+            <br>
+            <br>
+
+            <a href="index.php"><p id="footertitle"><i class='far fa-lightbulb' style='font-size:20px'></i> Eurêka</h1></a>
+
+        <br>
+        <br>
+
+        <center><a href="https://www.reddit.com/user/MyWare"><img src="reddit-logo-16.png" style="width:3%;"></a></center>
+
+        <br>
+        <br>
+
+        <nav><li><a href="index%20(english).php" id="langue"><span id="rouge">Change</span> <span id="blanc">language to</span> <span id="bleu">English</span> <i class='fas fa-globe-americas' style='font-size:13px'></i></a></li></nav>
+
+        <br>
+        <br>
+
+        <p id="copyright"><a href="https://www.youtube.com/watch?v=dQw4w9WgXcQ">&copy; 2021 Martin Droulez</a></p>
+
+        <br>
+        <br>
+        <br>
+        <br>
+        <br>
+        <br>
+        <br>
+        <br>
+        <br>
+        <br>
+        <br>
+        <br>
+        <br>
+        <br>
+        <br>
+
+        </footer>
+
+    <div class="ocean">
+        <div class="wave"></div>
+        <div class="wave"></div>
+    </div>
+
+    <!-- ----------------------------------------------------------------------------------------------------------------------------------------------------------------- -->
+
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/animejs/2.0.2/anime.min.js"></script>
+
+    <script>
+
+        var btncopy = document.querySelector('.js-copy');
+        if(btncopy) {
+            btncopy.addEventListener('click', docopy);
+        }
+
+        function docopy() {
+
+            // Cible de l'élément qui doit être copié
+            var target = this.dataset.target;
+            var fromElement = document.querySelector(target);
+            if(!fromElement) return;
+
+            // Sélection des caractères concernés
+            var range = document.createRange();
+            var selection = window.getSelection();
+            range.selectNode(fromElement);
+            selection.removeAllRanges();
+            selection.addRange(range);
+
+            try {
+                // Exécution de la commande de copie
+                var result = document.execCommand('copy');
+                if (result) {
+                    // La copie a réussi
+                    alert('Votre idée a été copiée dans le presse-papier');
+                }
+            }
+            catch(err) {
+                // Une erreur est surevnue lors de la tentative de copie
+                alert(err);
+            }
+
+            // Fin de l'opération
+            selection = window.getSelection();
+            if (typeof selection.removeRange === 'function') {
+                selection.removeRange(range);
+            } else if (typeof selection.removeAllRanges === 'function') {
+                selection.removeAllRanges();
+            }
+        }
+
+        /*-------------------------------------------------------------------------------------------------------------------------------------------------------------------*/
+
+        var textWrapper = document.querySelector('.ml1 .letters');
+        textWrapper.innerHTML = textWrapper.textContent.replace(/\S/g, "<span class='letter'>$&</span>");
+
+        anime.timeline({loop: false})
+            .add({
+            targets: '.ml1 .letter',
+            scale: [0.3,1],
+            opacity: [0,1],
+            translateZ: 0,
+            easing: "easeOutExpo",
+            duration: 600,
+            delay: (el, i) => 70 * (i+1)
+        }).add({
+            targets: '.ml1 .line',
+            scaleX: [0,1],
+            opacity: [0.5,1],
+            easing: "easeOutExpo",
+            duration: 700,
+            offset: '-=875',
+            delay: (el, i, l) => 80 * (l - i)
+        }).add({
+            targets: '.ml1',
+            opacity: 100,
+            duration: 1000,
+            easing: "easeOutExpo",
+            delay: 1000
+        });
+
+        /*-------------------------------------------------------------------------------------------------------------------------------------------------------------------*/
+
+        $(document).ready(function(){
+
+            $(".eureka").click(function(){
+                $("div#form").slideDown(1000);
+            });
+
+            $(".ml1").click(function(){
+                $("div#form").slideDown(1000);
+            });
+
+            $("#slideup").click(function(){
+                $("div#form").slideUp(1000);
+            });
+
+        });
+
+    </script>
+
+    </body>
+</html>
---
+++ mail (english).php
@@ -0,0 +1,254 @@
+<?php
+
+if(isset($_POST['idea']) OR isset($_POST['message'])){
+
+    if(isset($_POST['mailform'])){
+
+        if(!empty($_POST['mail'])){
+
+            $header="MIME-Version: 1.0\r\n";
+            $header.='From:"eurêka.com"'."\n";
+            $header.='Content-Type:text/html; charset="uft-8"'."\n";
+            $header.='Content-Transfer-Encoding: 8bit';
+
+            $message='
+<html>
+    <body>
+        <div align="center">
+
+            <h1 style="color: black;font-family: monospace;font-size: 30px;text-decoration: overline;">
+                <a href="#" style="text-decoration: none;color: black;">
+                    Eurêka
+                </a>
+            </h1>
+
+            <br>
+
+            <i style="font-family:monospace;">Sender\'s email :&nbsp;</i>'.$_POST['mail'].'<br />
+
+            <h1 style="font-family:monospace;"><u>Your idea :</u></h1>
+
+            <br>
+
+            <p style="font-family:monospace;font-size:30px;">"'.($_POST['message']).'"</p>
+
+            <br>
+
+            <h3 style="font-family:monospace;">What a good idea !</h3>
+
+            <br>
+            <br>
+
+            <footer>
+
+                <div style="height: 5px;width: 30%;background-color: black;"></div>
+
+                <br>
+
+                <a href="index.php" style="text-decoration: none;color: black;"><p style="font-family: monospace;font-size: 17px;text-decoration: overline;font-weight: 600;">Eurêka</h1></a>
+
+            <br>
+
+            <center><a href="https://www.reddit.com/user/MyWare">Reddit</a></center>
+
+            <br>
+
+            <p style="font-family: monospace;font-size: 12px;text-align: center;opacity: 70%;">&copy; 2021 Martin Droulez</p>
+
+            </footer>
+
+        </div>
+    </body>
+</html>
+';
+
+            mail($_POST['mail'], "Your idea - Eurêka", $message, $header);
+
+            $succes="<div class='succes'>Your idea has been sent to you <br> You will be redirected</div>";
+
+            header('refresh:2;url=index%20(english).php');
+
+        }else{
+            $erreur="<div class='erreur'>Please complete all fields</div>";
+        }
+    }
+
+?>
+
+<html lang="en">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Eurêka</title>
+        <link rel="stylesheet" href="style.css">
+        <script src='https://kit.fontawesome.com/a076d05399.js' crossorigin='anonymous'></script>
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+        <link rel="icon" type="image/png" href="favicon.JPG" />
+        <style>
+            html, body { height: 100%; }
+            body {
+                overflow: auto;
+            }
+
+            .ocean { 
+                height: 5%;
+                width:100%;
+                position:fixed;
+                bottom:0;
+                left:0;
+                background: #015871;
+                z-index: -10;
+            }
+
+            .wave {
+                background: url(https://s3-us-west-2.amazonaws.com/s.cdpn.io/85486/wave.svg) repeat-x; 
+                position: absolute;
+                top: -198px;
+                width: 6400px;
+                height: 198px;
+                animation: wave 7s cubic-bezier( 0.36, 0.45, 0.63, 0.53) infinite;
+                transform: translate3d(0, 0, 0);
+                z-index: -10;
+            }
+
+            .wave:nth-of-type(2) {
+                top: -175px;
+                animation: wave 7s cubic-bezier( 0.36, 0.45, 0.63, 0.53) -.125s infinite, swell 7s ease -1.25s infinite;
+                opacity: 1;
+            }
+
+            @keyframes wave {
+                0% {
+                    margin-left: 0;
+                }
+                100% {
+                    margin-left: -1600px;
+                }
+            }
+
+            @keyframes swell {
+                0%, 100% {
+                    transform: translate3d(0,-25px,0);
+                }
+                50% {
+                    transform: translate3d(0,5px,0);
+                }
+            }
+
+            div#vertical{
+                margin-top: 13%;
+            }
+
+            div.succes{
+                font-size: 20px;
+                color: green;
+                border-width: thin;
+                font-family: monospace;
+                text-align: center;
+                border-color: green;
+            }
+
+            div.erreur{
+                font-size: 20px;
+                color: red;
+                border-width: thin;
+                font-family: monospace;
+                text-align: center;
+                border-color: red;
+            }
+        </style>
+    </head>
+
+    <body>
+
+        <h1 class="title"><a href="index%20(english).php"><i class='far fa-lightbulb' style='font-size:36px'></i> Eurêka</a></h1>
+
+        <div id="vertical">
+
+            <?php
+
+    if(isset($succes)){
+        echo $succes;
+    }
+
+    if(isset($erreur)){
+        echo $erreur;
+    }
+
+            ?>
+
+            <form method="POST" action="">
+
+                <h2 id="note">Your idea <i class='fas fa-arrow-down' style='font-size:16px'></i></h2>
+
+                <input type="text" class="form" name="message" value="<?php if(isset($_POST['idea'])){ echo $_POST['idea']; }else{ echo $_POST['message']; } ?>" /><br><br>
+
+                <h2 id="note">Send your idea by email <i class='fas fa-terminal' style='font-size:16px'></i></h2>
+
+                <input type="email" class="form" name="mail" placeholder="Your email"/><br /><br />
+
+                <input type="submit" value="Send" name="mailform" class="form"/><br /><br />
+
+            </form>
+
+        </div>
+
+        <footer>
+
+            <div class="separation"></div>
+
+            <br>
+            <br>
+            <br>
+
+            <a href="index%20(english).php"><p id="footertitle"><i class='far fa-lightbulb' style='font-size:20px'></i> Eurêka</h1></a>
+
+        <br>
+        <br>
+
+        <center><a href="https://www.reddit.com/user/MyWare"><img src="reddit-logo-16.png" style="width:3%;"></a></center>
+
+        <br>
+        <br>
+
+        <nav><li><a href="index.php" id="langue"><span id="bleu">Changer la</span> <span id="blanc">langue en</span> <span id="rouge">Français</span> <i class='fas fa-globe-americas' style='font-size:13px'></i></a></li></nav>
+
+        <br>
+        <br>
+
+        <p id="copyright"><a href="https://www.youtube.com/watch?v=dQw4w9WgXcQ">&copy; 2021 Martin Droulez</a></p>
+
+        <br>
+        <br>
+        <br>
+        <br>
+        <br>
+        <br>
+        <br>
+        <br>
+        <br>
+        <br>
+        <br>
+        <br>
+        <br>
+        <br>
+        <br>
+
+        </footer>
+
+    <div class="ocean">
+        <div class="wave"></div>
+        <div class="wave"></div>
+    </div>
+
+    </body>
+</html>
+
+
+<?php
+
+}else{
+    header('Location:index.php');
+}
+
+?>
